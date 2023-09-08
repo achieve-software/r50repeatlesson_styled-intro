04a91b141097f472aa2e54c3c15957e8d11c3b04
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-08 12:34:57 +0300

    styled

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Button, FS13Button } from "./components/Buton.styled";
+
+const App = () => {
+  return (
+    <>
+      <h1>Styled Components</h1>
+      <Button> Send </Button>
+      <Button ab> Send </Button>
+      <FS13Button react> CALIS </FS13Button>
+      <FS13Button > DAHA COK CALIS </FS13Button>
+    </>
+  );
+};
+
+export default App;
